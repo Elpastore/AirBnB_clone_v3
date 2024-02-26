29dbdc25a9c035909ccaadcafd2c55191606bc13
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-02-26 21:24:23 +0100

    Update amenities.py

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -8,7 +8,8 @@ from models.amenity import Amenity
 @app_views.route('/amenities', methods=['GET'],
                  strict_slashes=False)
 def get_all():
-  """A function that gets all amenities from amenities class
+  """
+  A function that gets all amenities from amenities class
   """
   all_values = []
   obj = storage.all('Amenity').values()
@@ -19,7 +20,9 @@ def get_all():
 @app_views.route('/amenities/<amenity_id>', methods=['GET'],
                  strict_slashes=False)
 def get_by_id(amenity_id):
-  """A function that queries an amenity by it's id"""
+  """
+  A function that queries an amenity by it's id
+  """
   data = storage.get('Amenity', amenity_id)
   if data is None:
     abort(404)
@@ -28,7 +31,9 @@ def get_by_id(amenity_id):
 @app_views.route('/amenities/<amenity_id>', methods=['DELETE'],
                  strict_slashes=False)
 def delete_by_id(amenity_id):
-  """A function that deletes an amenity bi it's id"""
+  """
+  A function that deletes an amenity bi it's id
+  """
   obj = storage.get('Amenity', amenity_id)
   if obj is None:
     abort(404)
@@ -39,12 +44,14 @@ def delete_by_id(amenity_id):
 @app_views.route('/amenities', methods=['POST'],
                  strict_slashes=False)
 def create_amenity():
-  """A function that creates a new amenity"""
+  """
+  A function that creates a new amenity
+  """
   data = request.get_json()
   if data is None:
-    return make_response(jsonify({'error': 'Not a JSON'}), 400)
+    abort(400, 'Not a JSON')
   if 'name' not in data:
-    return make_response(jsonify({'error': 'Missing name'}), 400)
+    abort(400, 'Missing name')
   amenity = Amenity(**data)
   amenity.save()
   return jsonify(amenity.to_dict()), 201
@@ -52,13 +59,15 @@ def create_amenity():
 @app_views.route('/amenities/<amenity_id>', methods=['PUT'],
                  strict_slashes=False)
 def update_by_id(amenity_id):
-  """A function that updates the amenity"""
+  """
+  A function that updates the amenity
+  """
   amenity = storage.get('Amenity', amenity_id)
   if amenity is None:
-    return abort(400)
+    abort(404)
   data = request.get_json()
   if not data:
-    return make_response(jsonify({'error': 'Not a JSON'}), 400)
+    abort(400, 'Not a JSON')
   for key, value in data.items():
     if key not in ['id', 'created_at', 'updated_at']:
       setattr(amenity, key, value)
