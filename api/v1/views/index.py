c09eeb31ff509b6efbe43df3edb1522ad79b3bd1
elpastore <abdoulayesadio1997@gmail.com>
2024-02-26 16:05:28 +0000

    task4,5,6,7

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,7 +5,26 @@ from flask import Flask, Blueprint, jsonify
 from models import storage
 
 
+hbnb_features = {
+    'states': 'State',
+    'cities': 'City',
+    'users': 'User',
+    'places': 'Place',
+    'reviews': 'Review',
+    'amenities': 'Amenity'
+}
+
+
 @app_views.route('/status', strict_slashes=False)
 def status():
     """return a JSON status"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """endpoint that retrieves the number of each objects by type"""
+    stats_dict = {}
+    for key, values in hbnb_features.items():
+        stats_dict[key] = storage.count(values)
+    return jsonify(stats_dict)
