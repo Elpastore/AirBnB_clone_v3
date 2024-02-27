ac232c39b1abd5ffa4bdc6c4158fc05989f90e17
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-02-27 11:24:18 +0100

    fixed wrong string on line 39

--- api/v1/app.py
+++ api/v1/app.py
@@ -36,7 +36,7 @@ def error_handler(error):
 
 if __name__ == "__main__":
     app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    app_port = int(os.getenv('HBNB_API_PORT', '5000'))
+    app_port = int(os.getenv('HBNB_API_PORT', 5000))
     app.run(
         host=app_host,
         port=app_port,
